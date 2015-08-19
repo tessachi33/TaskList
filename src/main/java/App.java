c687a69be4ab572bcce2cd8f8c7e4f29f5b84d23
Tessa <Tessachi33@yahoo.com>
2015-08-18 20:40:26 -0700

    31 errors, too sick feeling to continue, WIP

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,12 +2,11 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    String layout = ("templates/layout.vtl");
+    String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
@@ -15,16 +14,34 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-
-    post("/Task", (request, response) -> {
+    get("/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("tasks/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+    post("/tasks", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
       String description = request.queryParams("description");
       Task newTask = new Task(description);
-      request.session().attribute("Task", newTask);
-
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
-}
+  get("/tasks/:id", (request, response) -> {
+  HashMap<String, Object> model = new HashMap<String, Object>();
+
+  Task task = Task.find(Integer.parseInt(request.params(":id")));
+  model.put("task", task);
+  model.put("template", "templates/task.vtl");
+  return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+ }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,62 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Task {
-   String mDescription;
+  private static ArrayList<Task> instances = new ArrayList<Task>();
+
+  private String mDescription;
+  private LocalDateTime mCreatedAt;
+  private boolean mCompleted;
+  private int mId;
 
   public Task(String description) {
-  mDescription = description;
+    mDescription = description;
+    mCreatedAt = LocalDateTime.now();
+    mCompleted = false;
+    instances.add(this);
+    mId = instances.size();
   }
 
-  public String getDescription(){
+  public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public void completeTask() {
+    mCompleted = true;
+  }
+
+  public static ArrayList<Task> all() {
+    return instances;
+  }
+
+  public static Task find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException e) {
+      return null;
+    }
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  instances.add(this);
+  mId = instances.size();
+  
+  public static Task find(int id) {
+    return instances.get(id - 1);
   }
 }
